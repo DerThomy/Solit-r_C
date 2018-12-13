07ac027bf411bb0029e08521c10f3b0df44873dd
Piakor <45855714+Piakor@users.noreply.github.com>
2018-12-13 18:32:48 +0100

    Add files via upload

---
+++ ass3/ass3.c
@@ -0,0 +1,22 @@
+//-----------------------------------------------------------------------------
+// ass3.c
+//
+// Solitaire
+//
+// Group: - study assistant Mathias Kahr
+//
+// Authors: Simon Joham 11814175
+//  Thomas Köhler #matNr
+//
+// Latest Changes: 13.12.2018 (by Simon Joham)
+//-----------------------------------------------------------------------------
+//
+
+
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  return 0;
+}
---
+++ ass3/cmake-build-debug/CMakeFiles/ass3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass3.dir/ass3.c.obj"
+  "ass3.pdb"
+  "ass3.exe"
+  "ass3.exe.manifest"
+  "ass3.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ass3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
