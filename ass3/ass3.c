e1409bcbbf22a93a995baaaf1ba0712d0b676835
Piakor <johamsimon@gmx.at>
2018-12-13 21:01:39 +0100

    Version 0.02
    Erste Implementierung des Karteneinlesens.

--- ass3/ass3.c
+++ ass3/ass3.c
@@ -12,11 +12,84 @@
 //-----------------------------------------------------------------------------
 //
 
-
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, World!\n");
+//Return values of the program
+typedef enum _ReturnValue_
+{
+  EVERYTHING_OK = 0,
+  INVALID_ARGUMENTS = 1,
+  OUT_OF_MEMORY = 2,
+  INVALID_FILE = 3
+} ReturnValue;
+
+enum CardValue
+{
+   A = 1,
+   J = 11,
+   Q = 12,
+   K = 13
+};
+
+
+ReturnValue printErrorMessage(ReturnValue return_value);
+ReturnValue readCardsFromPath(char *path);
+ReturnValue readCardsFromFromFile(FILE* file);
+
+int main(int argc, char **argv)
+{
+  printf("%d\n",argc);
+
+  if(argc != 2)
+  {
+    return printErrorMessage(INVALID_ARGUMENTS);
+  }
+
   return 0;
+}
+
+ReturnValue readCardsFromPath(char *path)
+{
+  FILE* file = fopen(path, "r");
+  if(file == NULL)
+  {
+    return INVALID_FILE;
+  }
+  ReturnValue read_return_value = readCardsFromFromFile(file);
+
+  fclose(file);
+  return read_return_value;
+}
+
+ReturnValue readCardsFromFromFile(FILE* file)
+{
+
+}
+
+
+
+//------------------------------------------------------------------------------
+///
+/// Print message which describes the return value.
+///
+/// @param return_value type of main return value
+///
+/// @return parameter return_value
+//
+ReturnValue printErrorMessage(ReturnValue return_value)
+{
+  switch(return_value)
+  {
+    case INVALID_ARGUMENTS:
+      printf("[ERR] Usage: ./ass3 [file-name]\n");
+      break;
+    case OUT_OF_MEMORY:
+      printf("[ERR] Out of memory.\n");
+      break;
+    case INVALID_FILE:
+      printf("[ERR] Invalid file!\n");
+      break;
+  }
+  return return_value;
 }
