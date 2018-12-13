ac2c1f6efb752d87d9193be5446c028821e7d594
Piakor <johamsimon@gmx.at>
2018-12-13 19:40:53 +0100

    Config.txt hinzugefügt

--- ass3/cmake-build-debug/CMakeFiles/ass3.dir/DependInfo.cmake
+++ ass3/cmake-build-debug/CMakeFiles/ass3.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Simon/CLionProjects/ass3/ass3.c" "C:/Users/Simon/CLionProjects/ass3/cmake-build-debug/CMakeFiles/ass3.dir/ass3.c.obj"
+  "C:/Users/Simon/CLionProjects/Solitaer_C/ass3/ass3.c" "C:/Users/Simon/CLionProjects/Solitaer_C/ass3/cmake-build-debug/CMakeFiles/ass3.dir/ass3.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "MSVC")
 
--- ass3/cmake-build-debug/cmake_install.cmake
+++ ass3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Simon/CLionProjects/ass3
+# Install script for directory: C:/Users/Simon/CLionProjects/Solitaer_C/ass3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Simon/CLionProjects/ass3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Simon/CLionProjects/Solitaer_C/ass3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
