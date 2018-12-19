d521c7e3ea9d5387c43747481bea82f683348069
Simon <johamsimon@gmx.at>
2018-12-19 18:35:44 +0100

    AddTop ist nun doppelt verkettet

--- ass3/ass3.c
+++ ass3/ass3.c
@@ -150,9 +150,8 @@ void addTop(Card **top, char color, char *value)
 
 
   new_card->next = *top;    // next points to previous top card
-
   *top = new_card; // top now points to new card
-  if(new_card->next != NULL)
+  if(new_card->next != NULL) //if top was already a card, the previous card of the old top is new card
   {
     (new_card->next)->previous = new_card;
   }
